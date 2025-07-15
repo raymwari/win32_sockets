4a3bfae7e0792c89a4f3bb24b85e75c2c03f41a8
raymwari <mwari.ray@gmail.com>
2025-07-14 23:34:56 -0700

    Delete winapi_win_example/src directory

--- winapi_win_example/src/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if exist main.exe (del main.exe main.obj)
-nasm -f win32 main.asm
-nasm -f win32 proxy.asm
-nasm -f win32 forward.asm
-if exist main.obj (golink /console /entry _main main.obj proxy.obj forward.obj kernel32.dll ws2_32.dll user32.dll)
-if exist main.obj (del main.obj proxy.obj forward.obj)
-if exist main.exe (main.exe)
